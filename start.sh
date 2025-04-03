76f5c9ae30278deea94c82c43f57003efc0811a1
Denis Dragomirik <denis@dragomirik.com>
2025-04-04 01:41:05 +0300

    Create docker-compose file
    Encapsulate container in docker-compose configuration in order to fix the issue with create a new docker-container every time on restarting

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker build -t rel-alg .
-docker ps | grep 4568 | awk '{print $1}' | xargs -r docker stop
-docker run -p 4568:4567 -v "$(pwd)/data:/rel-alg/data" rel-alg
+# Stop any existing containers and remove them
+docker-compose down
+
+# Build and start the service
+docker-compose up --build
